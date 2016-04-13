c9e9669f9744e2d4c833e7f3eeb870f30c985144
Darius <darius@adroiti.com>
2016-04-13 13:52:07 +0300

    initial

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cf_style_csharp_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Test: {1}");
+
+
+        }
+    }
+}
